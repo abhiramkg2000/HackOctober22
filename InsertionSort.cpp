f8e548890ee5b2afa5a7de1f2818db24bf692319
Alok Sagar <87970098+Alok5445Sagar@users.noreply.github.com>
2022-10-08 09:05:50 +0530

    Selection, Insertion & Merge Sort

---
+++ InsertionSort.cpp
@@ -0,0 +1,37 @@
+/*C++ Programme for the insertion sort*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void InsertionSort(int arr[], int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    InsertionSort(arr, n);
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+
+}

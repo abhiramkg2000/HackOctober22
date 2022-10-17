704581b57a2b04176333371d90cd012a0192e3f8
Shweta jha <85930909+shwetaj954@users.noreply.github.com>
2022-10-17 15:21:00 +0530

    Update ChangeTwoDigitInNumberInEnglish.c

--- C programs/ChangeTwoDigitInNumberInEnglish.c
+++ C programs/ChangeTwoDigitInNumberInEnglish.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+int main()
+{
 	int n, t;
 	char n1[][11] = { "Ten","Eleven","Twelve","Thirteen","Fourteen","Sixteen","Seventeen","Eighteen","Nineteen" };
 	char n2[][10] = { "One","Two","Three","Four","Five","Six","Seven","Eight","Nine" };
@@ -9,19 +12,21 @@
 		printf("Please Enter the valid Two Digit Number");
 	}
 	else {
-    if (n < 20) {
-      t = n % 10;
-      printf("%s",n1[t]);
-    }
-    else if (n >=20){
-      t = n % 10;
-      int t1 = n / 10;
-      if (n % 10 == 0)
-      {
-        printf("%s", n3[t1 - 2]);
-      }
-      else {
-        printf("%s %s", n3[t1 - 2], n2[t-1]);
-      }
-    }
+	    if (n < 20) {
+	      t = n % 10;
+	      printf("%s",n1[t]);
+	    }
+	    else if (n >=20){
+	      t = n % 10;
+	      int t1 = n / 10;
+	      if (n % 10 == 0)
+	      {
+		printf("%s", n3[t1 - 2]);
+	      }
+	      else {
+		printf("%s %s", n3[t1 - 2], n2[t-1]);
+	      }
+	    }
 	}
+return 0;
+}

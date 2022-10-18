6a0401fe6dba83d9f0999c7fa60d55b85a6ca665
PratikMoharana <93613757+PratikMoharana@users.noreply.github.com>
2022-10-18 22:11:51 +0530

    Calculator.c
    Program to Make a Simple Calculator

---
+++ C programs/calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	char ch;
+	double a, b;
+	while (1) {
+		printf("Enter an operator (+, -, *, /), if want to exit press x: ");
+		scanf(" %c", &ch);
+		if (ch == 'x')
+			exit(0);
+		printf("Enter two first and second operand: ");
+		scanf("%lf %lf",&a,&b);
+		switch (ch) {
+		case '+':
+			printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+			break;
+		case '-':
+			printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
+			break;
+		case '*':
+			printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
+			break;
+		case '/':
+			printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
+			break;
+		default:
+			printf("Error! please write a valid operator\n");
+		}
+	}
+}
